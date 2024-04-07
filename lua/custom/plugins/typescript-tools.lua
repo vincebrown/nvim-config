16e92cf7aeeedcbafa320b47d60852d4e304ce4d
Vince Brown <vince.brown@mac-f3xvhrhkgh.lan>
2024-04-07 11:57:22 -0700

    Initial commit, moving plugins to individual files

---
+++ lua/custom/plugins/typescript-tools.lua
@@ -0,0 +1,8 @@
+return {
+  -- Typescript Language Support
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {},
+  },
+}
