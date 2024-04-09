15cb625d5237f8c4fb7d6897642a9cbfe3d87eeb
Vince Brown <vince.brown@hingehealth.com>
2024-04-09 06:42:34 -0700

    Added keybind for zenmode

--- lua/custom/plugins/zen-mode.lua
+++ lua/custom/plugins/zen-mode.lua
@@ -6,6 +6,10 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      relativenumber = true,
     },
+    config = function()
+      vim.keymap.set('n', '<leader>zm', ':ZenMode<CR>', { desc = '[Z]en [M]ode Toggle' })
+    end,
   },
 }
