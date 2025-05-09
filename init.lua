d861685b1eb5d85d1483cd807cbc132d4403c80c
Vince Brown <vince.brown@hingehealth.com>
2025-05-08 22:28:10 -0700

    Removed VSCode settings
    No longer going to use cursor can remove vscode settings

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+require 'options'
+require 'keymaps'
+require 'autocommands'
+
+vim.cmd.colorscheme 'catppuccin-mocha'
+
 -- Initialize Lazy
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
@@ -11,21 +17,6 @@ end ---@diagnostic disable-next-line: undefined-field
 
 vim.opt.rtp:prepend(lazypath)
 
-if vim.g.vscode then
-  require 'vscode-keymaps'
-  require('lazy').setup {
-    { import = 'vscode-plugins' },
-  }
-  -- VSCode extension
-else
-  require 'options'
-  require 'keymaps'
-  require 'autocommands'
-
-  require('lazy').setup {
-    { import = 'plugins' },
-  }
-
-  -- Set Colorscheme
-  vim.cmd.colorscheme 'catppuccin-mocha'
-end
+require('lazy').setup {
+  { import = 'plugins' },
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,9 @@
 local set = vim.keymap.set
 
-set('n', '<c-j>', '<c-w><c-j>')
-set('n', '<c-k>', '<c-w><c-k>')
-set('n', '<c-l>', '<c-w><c-l>')
-set('n', '<c-h>', '<c-w><c-h>')
+set('n', '<C-h>', '<C-w>h', { desc = 'Switch window left' })
+set('n', '<C-l>', '<C-w>l', { desc = 'Switch window right' })
+set('n', '<C-j>', '<C-w>j', { desc = 'Switch window down' })
+set('n', '<C-k>', '<C-w>k', { desc = 'Switch window up' })
 
--- set('n', '<leader>x', '<cmd>.lua<CR>', { desc = 'Execute the current line' })
--- set('n', '<leader><leader>x', '<cmd>source %<CR>', { desc = 'Execute the current file' })
-
-set('n', '<leader>w', '<cmd>silent! w<CR>', { desc = 'save current buffer' })
-set('n', '<leader>q', '<cmd>q<cr>', { desc = 'close neovim' })
+set('n', '<leader>w', '<cmd>silent! w<CR>', { desc = 'Save current buffer' })
+set('n', '<leader>q', '<cmd>q<cr>', { desc = 'Close neovim' })
