3a1650fb186bec9effc451aea59d1e7e87e460f2
Vince Brown <vince.brown@hingehealth.com>
2025-11-11 18:07:53 -0800

    Added tailwind lsp and autotag

---
+++ lsp/tailwindcss.lua
@@ -0,0 +1,63 @@
+local utils = require 'core.utils'
+
+return {
+  cmd = { 'tailwindcss-language-server', '--stdio' },
+  root_markers = {
+    'vite.config.ts',
+    'tailwind.config.js',
+    'tailwind.config.cjs',
+    'tailwind.config.mjs',
+    'tailwind.config.ts',
+    'postcss.config.js',
+    'postcss.config.cjs',
+    'postcss.config.mjs',
+    'postcss.config.ts',
+  },
+  filetypes = {
+    -- html
+    'astro',
+    'astro-markdown',
+    'gohtml',
+    'gohtmltmpl',
+    'html',
+    'markdown',
+    'mdx',
+    'mustache',
+    -- css
+    'css',
+    'postcss',
+    -- js
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+  },
+  settings = {
+    tailwindCSS = {
+      validate = true,
+      lint = {
+        cssConflict = 'warning',
+        invalidApply = 'error',
+        invalidScreen = 'error',
+        invalidVariant = 'error',
+        invalidConfigPath = 'error',
+        invalidTailwindDirective = 'error',
+        recommendedVariantOrder = 'warning',
+      },
+      classAttributes = {
+        'class',
+        'className',
+        'class:list',
+        'classList',
+        'ngClass',
+      },
+      includeLanguages = {
+        eelixir = 'html-eex',
+        eruby = 'erb',
+        templ = 'html',
+        htmlangular = 'html',
+      },
+    },
+  },
+  capabilities = utils.create_lsp_capabilities(),
+}
