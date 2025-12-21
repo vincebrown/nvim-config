0cd8af628d2ece5118587464e2d07f3bc23c8a2e
Vince Brown <vince.brown@hingehealth.com>
2025-12-20 22:40:34 -0800

    Update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,11 +3,12 @@ return {
     'stevearc/oil.nvim',
     dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
     config = function()
-      CustomOilBar = function()
-        local path = vim.fn.expand '%'
-        path = path:gsub('oil://', '')
-
-        return '  ' .. vim.fn.fnamemodify(path, ':.')
+      local function get_oil_winbar()
+        local dir = require('oil').get_current_dir()
+        if dir then
+          return '  ' .. vim.fn.fnamemodify(dir, ':.')
+        end
+        return ''
       end
 
       require('oil').setup {
@@ -18,34 +19,37 @@ return {
           ['<C-k>'] = false,
           ['<C-j>'] = false,
           ['<M-h>'] = 'actions.select_split',
-          ['yp'] = function()
-            local oil = require 'oil'
-            local entry = oil.get_cursor_entry()
-            if not entry or not entry.name then
-              vim.notify('No entry found', vim.log.levels.WARN)
-              return
-            end
+          ['yp'] = {
+            desc = 'Yank relative path',
+            callback = function()
+              local oil = require 'oil'
+              local entry = oil.get_cursor_entry()
+              if not entry or not entry.name then
+                Snacks.notify.warn 'No entry found'
+                return
+              end
 
-            local dir = oil.get_current_dir()
-            local full_path = vim.fs.normalize(dir .. '/' .. entry.name)
-            local cwd = vim.fn.getcwd()
-            local relative_path = vim.fn.fnamemodify(full_path, ':.')
+              local dir = oil.get_current_dir()
+              local full_path = vim.fs.normalize(dir .. '/' .. entry.name)
+              local relative_path = vim.fn.fnamemodify(full_path, ':.')
 
-            vim.fn.setreg('+', relative_path)
-            vim.notify('Yanked relative path: ' .. relative_path)
-          end,
-        },
-        win_options = {
-          winbar = '%{v:lua.CustomOilBar()}',
+              vim.fn.setreg('+', relative_path)
+              Snacks.notify.info('Yanked: ' .. relative_path)
+            end,
+          },
         },
         view_options = {
           show_hidden = true,
-          is_always_hidden = function(name, _)
-            local folder_skip = { 'dev-tools.locks', 'dune.lock', '_build' }
-            return vim.tbl_contains(folder_skip, name)
-          end,
         },
       }
+
+      -- Set winbar for oil buffers using autocommand
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'oil',
+        callback = function()
+          vim.opt_local.winbar = get_oil_winbar()
+        end,
+      })
 
       -- Open parent directory in current window
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
